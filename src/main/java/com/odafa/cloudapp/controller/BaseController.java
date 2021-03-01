e4552ef0d8c9e7631d1774c2eb86a9a72b640e4a
Peter Petkanov <peter_black@angelic.com>
2021-03-01 21:05:39 +0200

    logging setup

--- src/main/java/com/odafa/cloudapp/controller/BaseController.java
+++ src/main/java/com/odafa/cloudapp/controller/BaseController.java
@@ -3,11 +3,16 @@ package com.odafa.cloudapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class BaseController {
     
     @GetMapping("/")
     public String indexPage() {
+        
+        log.debug("Index Page Opened");
         return "index";
     }
 }
